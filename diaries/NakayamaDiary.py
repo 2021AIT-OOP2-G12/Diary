259926bc4f80130a1f6cf23c4d9cba55f004d3fa
ryunosukenakayama <k19076kk@aitech.ac.jp>
2021-12-09 16:06:40 +0900

    中山日記追加

---
+++ diaries/NakayamaDiary.py
@@ -0,0 +1,12 @@
+from diaries.AbstractDiary import AbstractDiary
+
+class Nakayamadiary(AbstractDiary):
+
+    def get_date(self):
+        return "2021-12-09"
+
+    def get_summary(self):
+        return "グループワーク初日だった。リーダ決めと初期設定などを行った。"
+
+    def get_author(self):
+        return "Nakayama"
