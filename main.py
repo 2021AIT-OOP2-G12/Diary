da725b828edef92fa03d325531e56ff262166e81
Yokoyama-Yuichi <95839549+Yokoyama-Yuichi@users.noreply.github.com>
2021-12-09 16:00:42 +0900

    add diary

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from diaries.DiarySample import DiarySample
+from diaries.YokoyamaDiary import YokoyamaDiary
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
+  YokoyamaDiary()
 ] 
 
 for d in diaries:
