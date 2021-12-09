72ed91024372c033bbaccba18f8f86d84767dc7c
sagiiina <95839350+sagiiina@users.noreply.github.com>
2021-12-09 16:15:30 +0900

    add diary

---
+++ main.py
@@ -0,0 +1,15 @@
+from diaries.DiarySample import DiarySample
+from diaries.TamakoshiDiary import TamakoshiDiary
+
+# ↓のリストには、メンバーの各日記が格納されます。
+diaries = [
+  DiarySample(),
+  TamakoshiDiary()
+]
+
+for d in diaries:
+    print("---------------------------------")
+    print(d.get_date())
+    print(d.get_summary())
+    print(d.get_author())
+    print()
