944b46a4d913fb87fd7bae2af62c6695dd6f6c62
ryunosukenakayama <k19076kk@aitech.ac.jp>
2021-12-11 01:44:19 +0900

    再変更

--- diaries/TamakoshiDiary.py
+++ diaries/TamakoshiDiary.py
@@ -6,11 +6,7 @@ class TamakoshiDiary(AbstractDiary):
         return "2021-12-09"
 
     def get_summary(self):
-<<<<<<< HEAD
-        return "グループワーク初日緊張した。"
-=======
         return "グループワーク初日緊張した"
->>>>>>> 94fb2e5047d923be6eb97c9691d7bc63f4983be5
 
     def get_author(self):
         return "Tamakoshi"
