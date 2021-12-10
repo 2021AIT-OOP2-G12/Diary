e2d5fda7a2549c1622c89964d1991f6e5b4e93fc
ryunosukenakayama <k19076kk@aitech.ac.jp>
2021-12-11 01:40:42 +0900

    変更しました

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 from diaries.DiarySample import DiarySample
-<<<<<<< HEAD
-from diaries.TamakoshiDiary import TamakoshiDiary
-=======
 from diaries.YokoyamaDiary import YokoyamaDiary
 from diaries.ToyoshimaDiary import ToyoshimaDiary
 from diaries.TamakoshiDiary import TamakoshiDiary
 from diaries.SugisakaDiary import SugisakaDiary
 from diaries.EvafirstDiary import EvafirstDiary
 from diaries.NakayamaDiary import NakayamaDiary
->>>>>>> 94fb2e5047d923be6eb97c9691d7bc63f4983be5
 
 # ↓のリストには、メンバーの各日記が格納されます。
 diaries = [
   DiarySample(),
-<<<<<<< HEAD
-  TamakoshiDiary()
-]
-=======
   YokoyamaDiary(),
   ToyoshimaDiary(),
   TamakoshiDiary(),
@@ -24,7 +16,6 @@ diaries = [
   EvafirstDiary(),
   NakayamaDiary()
 ] 
->>>>>>> 94fb2e5047d923be6eb97c9691d7bc63f4983be5
 
 for d in diaries:
     print("---------------------------------")
